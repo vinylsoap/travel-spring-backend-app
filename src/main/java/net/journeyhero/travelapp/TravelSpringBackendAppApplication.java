47cf450f6516bf8515c07b8b393633dfdb8f3878
vinylsoap <lioppororoka@gmail.com>
2025-06-29 18:27:25 +0200

    spring boot init  project

---
+++ src/main/java/net/journeyhero/travelapp/TravelSpringBackendAppApplication.java
@@ -0,0 +1,13 @@
+package net.journeyhero.travelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpringBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSpringBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/journeyhero/travelapp/TravelSpringBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.journeyhero.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSpringBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
