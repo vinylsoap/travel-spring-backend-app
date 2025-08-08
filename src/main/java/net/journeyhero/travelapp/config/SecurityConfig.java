5c679644c929e27aea413ca1f344e09c4f763b5a
vinylsoap <lioppororoka@gmail.com>
2025-08-08 18:57:52 +0200

    allowed CORS

--- src/main/java/net/journeyhero/travelapp/config/SecurityConfig.java
+++ src/main/java/net/journeyhero/travelapp/config/SecurityConfig.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -22,6 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
         http.sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(customizer -> customizer
                         .requestMatchers(HttpMethod.GET, "/api/attraction").permitAll()
                         .anyRequest().authenticated()
@@ -33,5 +39,15 @@ public class SecurityConfig {
                 });
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(@Value("#{'${cors.allowed-origins}'.split(',')}") List<String> allowedOrigins) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigins.toArray(new String[0]));
+            }
+        };
     }
 }
