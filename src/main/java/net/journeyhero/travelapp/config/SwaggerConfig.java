7faf0c0f060f61c23dc274e173045435f9975d55
vinylsoap <lioppororoka@gmail.com>
2025-08-19 20:08:15 +0200

    Force https in Swagger UI

--- src/main/java/net/journeyhero/travelapp/config/SwaggerConfig.java
+++ src/main/java/net/journeyhero/travelapp/config/SwaggerConfig.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 @SecurityScheme(name = "Auth0", type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(authorizationCode = @OAuthFlow(tokenUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}oauth/token", authorizationUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}authorize")))
 public class SwaggerConfig {
